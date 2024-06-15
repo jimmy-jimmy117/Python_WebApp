ca18053eacc0d63e0765a11700bd8a87e6324a30
nakahara <OCA2204240019@edu.oca.ac.jp>
2024-06-15 14:54:37 +0900

    リクエストデータの取得　演習の完了

--- app.py
+++ app.py
@@ -16,3 +16,8 @@ def inex2():
 @app.route("/test_request")
 def test_request():
     return f'test_request:{request.args.get("dummy")}'
+
+
+@app.route("/exercise_request/<dummy>")
+def test_request2(dummy):
+    return f"exercise_request;{dummy}"
